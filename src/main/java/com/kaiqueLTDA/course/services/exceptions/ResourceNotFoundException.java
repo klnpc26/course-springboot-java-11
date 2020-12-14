a5b2600b03b2952ad62b8f86e460ac9594c7666a
kaique <kaique@gmail.com>
2020-12-14 19:02:08 -0300

    Exception handling - findById

---
+++ src/main/java/com/kaiqueLTDA/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.kaiqueLTDA.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
